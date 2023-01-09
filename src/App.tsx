18f6f05e336df94b924fb436c1988ec6c1aec26c
Ariel Barkan <arielbarkan@gmail.com>
2023-01-10 00:11:33 +0200

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { clear } from 'console';
+import React, { ChangeEventHandler } from 'react';
+
 
 function App() {
+
+
+  const validateIcon = async (): Promise<any> => {
+    return new Promise(async (resolve, reject) => {
+      resolve("hello world")
+    })
+
+  }
+  const handleConfigureAppBasicData = async (event: React.ChangeEvent<HTMLInputElement>): Promise<void> => {
+    console.log("---------BEFORE------------------START----------------")
+    console.log("1 - event.target.name = ", event.target.name);
+    console.log("2 - event.target.files = ", event.target.files);
+    console.log("3 - event.target.files![0] = ", event.target.files![0]);
+    console.log("4 - event.target.files![0].size = ", event.target.files![0].size);
+    console.log("---------BEFORE------------------END----------------")
+
+    const someVar: any = await validateIcon()
+    console.log(someVar)
+
+    console.log("---------AFTER------------------START----------------")
+    console.log("1 - event.target.name = ", event.target.name);
+    console.log("2 - event.target.files = ", event.target.files);
+    console.log("3 - event.target.files![0] = ", event.target.files![0]);
+    console.log("4 - event.target.files![0].size = ", event.target.files![0].size);
+    console.log("---------AFTER------------------END----------------")
+
+  }
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form>
+
+        <input type="file" id="icon" name="icon" onChange={handleConfigureAppBasicData} />
+      </form>
+
     </div>
   );
 }
